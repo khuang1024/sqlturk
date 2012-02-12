638c5f1315f96d4bfa5130fd1120bb77295c4627
khuang1024 <khuang1024@gmail.com>
2012-02-12 15:24:29 -0800

    randomly choose n elements and rank them
    This script randomly picks up n elements from the original 10 elements.
    Then, it uses median rank to rank the n elements and returns the final
    rank. Run it like:
    
    python3 median_rank_parser.py 3 # randomly choose 3 items and rank them

--- median_rank_parser.py
+++ median_rank_parser.py
@@ -10,6 +10,10 @@ def extract_into_list(hit_id, qnum):
     '''This function parses the answers by given the HIT ID and the question
     number. Then, it returns a 2-dimensional list containing all these
     answers.'''
+    if type(qnum) == str:
+        qno = qnum
+    else:
+        qno = str(qnum)
     ranks = []
     f = open ("ranking_answer_log.csv")
     lines = f.read().split("\n")
@@ -17,7 +21,7 @@ def extract_into_list(hit_id, qnum):
         if re.match(hit_id, line): # match is better than search here
             fields = re.split("&", line)
             for field in fields:
-                if re.search("q"+qnum+"\[\]=", field): # field = p1[]=1|2|3..
+                if re.search("q"+qno+"\[\]=", field): # field = p1[]=1|2|3..
                     ranks.append(parse_into_list(re.split("=", field)[1]))
     return ranks
 
@@ -39,15 +43,15 @@ def atom_median_rank(ranks):
     record = {} # the record of how many times an element appears so far
     addition = {} # the record of additional info for sovlving"conflict"
     
-    for i in range(len(ranks[0])):
-        keys.append(str(i+1))
+    for i in ranks[0]:
+        keys.append(i)
 
-    for i in keys:  
+    for i in keys: 
         record.setdefault(i, 0)
         addition.setdefault(i,0)
     # record = dict.fromkeys(["1","2","3","4","5","6","7","8","9","10"], 0)
 
-    for i in range(len(ranks[0])):
+    for i in range(len(keys)):
         temp = {} 
 
         for rank in ranks:
@@ -61,7 +65,6 @@ def atom_median_rank(ranks):
                 temp[j] = record[j]
 
         for v in sorted(temp.values()):
-
             if list(temp.values()).count(v) == 1:
                 for k in temp.keys():
                     if temp[k] == v and k not in result:
@@ -76,15 +79,37 @@ def atom_median_rank(ranks):
                     result.append(again[k])
         
     return result
-                
-def median_rank(hit_id):
-    '''This function runs the median rank algorithm for a single HIT, including
-    both the first and second questions of a HIT.'''
-    ranks_p1 = extract_into_list(hit_id, "1")
-    ranks_p2 = extract_into_list(hit_id, "2")
-    print(hit_id + "\tq1\t" + str(atom_median_rank(ranks_p1)))
-    print(hit_id + "\tq2\t" + str(atom_median_rank(ranks_p2)))
-    print("------------------------------------------------------------------")
+
+def random_keys(n):
+    '''This function randomly generate some keys between 1 and 10. The number
+    of these random keys is indicated by n.'''
+    if type(n) == int:
+        num = n
+    else:
+        num = int(n)
+
+    keys = [];
+    while len(keys) < num:
+        random_num = str(random.randint(1, 10))
+        if random_num not in keys:
+            keys.append(random_num)
+
+    return keys
+
+def filter_ranks(ranks, keys):
+    '''This function accepts the ranks (the original 2-dimensional ranks) and
+    a list of keys. It generates a new 2-dimensional list which only keeps the
+    elements in keys. The new 2-dimensional list also keeps the original order
+    of these elements.'''
+    new_ranks = []
+    for rank in ranks:
+        new_rank = []
+        for key in rank:
+            if key in keys:
+                new_rank.append(key)
+        new_ranks.append(new_rank)
+    return new_ranks
+        
 
 def get_all_hits():
     '''This function returns all the HIT IDs in the answer log file.'''
@@ -96,17 +121,33 @@ def get_all_hits():
         if hit_id not in hit_ids and hit_id:
             hit_ids.append(hit_id)
     return hit_ids
+                
+def median_rank(hit_id, top):
+    '''This function runs the median rank algorithm for a single HIT, including
+    both the first and second questions of a HIT.'''
+    ranks_p1 = filter_ranks(extract_into_list(hit_id, "1"), random_keys(top))
+    ranks_p2 = filter_ranks(extract_into_list(hit_id, "2"), random_keys(top))
+    #print(ranks_p1)
+    #print(ranks_p2)
+    print(hit_id + "\tq1\t" + str(atom_median_rank(ranks_p1)))
+    print(hit_id + "\tq2\t" + str(atom_median_rank(ranks_p2)))
+    print("------------------------------------------------------------------")
 
 if __name__ == "__main__":
     import sys
     import re
+    import random
     hits = get_all_hits()
     for hit in hits:
-        median_rank(hit)
+        median_rank(hit, sys.argv[1])
 
+
+    # test
+    #rand = random_keys(3)
+    #origin = extract_into_list("2J5JKM05BMJTOZKMPXKPDRA8V1CDI", 1)
+    #ranks = filter_ranks(origin, rand)
+    #print(ranks)
     #print(get_all_hits())
     #ranks = extract_into_list(sys.argv[1], "1")
     #result = atom_median_rank(ranks)
     #print (result)
-    #test = [["2","1","3","4"],["3","1","4","2"]]
-    #print (atom_median_rank(test))
