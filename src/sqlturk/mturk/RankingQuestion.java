7b79819f4e9861690ddbfc43f05f44ccc5288bf3
khuang1024 <khuang1024@gmail.com>
2012-02-13 03:46:54 -0800

    renamed vote.php
    changed the name "vote.php" to "Vote.php"

--- src/sqlturk/mturk/RankingQuestion.java
+++ src/sqlturk/mturk/RankingQuestion.java
@@ -21,7 +21,7 @@ public class RankingQuestion {
 	    int questionIndexWithSample, int execMode, ArrayList<String> candidateQueries) throws IOException {
 	question = "<?xml version=\"1.0\"?>\n"; // NOTE: cannot use question +=
 	question += "<ExternalQuestion xmlns=\"http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2006-07-14/ExternalQuestion.xsd\">\n";
-	question += "<ExternalURL>http://users.soe.ucsc.edu/~khuang/vote.php?";
+	question += "<ExternalURL>http://users.soe.ucsc.edu/~khuang/Vote.php?";
 	question += "rels=" + getEncodedRelation(datasetName);
 	question += "&#x26;nlQuery1="
 		+ getEncodedQueryNL(datasetName, questionIndexWithoutSample);
