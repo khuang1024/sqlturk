f73d33fe48b85de3d9e86b2f087497a57d7c44eb
khuang1024 <khuang1024@gmail.com>
2012-02-14 19:51:16 -0800

    Commented out invalid queries

--- src/sqlturk/experiment/mturk/CreateRankingHITs.java
+++ src/sqlturk/experiment/mturk/CreateRankingHITs.java
@@ -92,7 +92,7 @@ public class CreateRankingHITs {
 	worldCandidateQueries23.add("select c2.name Country, c1.name, Capital  from City c1, Country c2  where c1.ID=c2.Capital");
 	// without sample
 	worldCandidateQueries23.add("select a.Name,b.Language from Country a, CountryLanguage b where a.Code = b.CountryCode");
-	worldCandidateQueries23.add("select Name from City where CountryCode in(select Language from CountryLanguage)"); // // the query is syntactically right and can be rewritten, can we have its provenance? and do we need to rewrite the sub-SPJ?
+	//worldCandidateQueries23.add("select Name from City where CountryCode in(select Language from CountryLanguage)"); // // the query is syntactically right and can be rewritten, can we have its provenance? and do we need to rewrite the sub-SPJ?
 	worldCandidateQueries23.add("select Country.Name,CountryLanguage.Language from Country,CountryLanguage where Country.Code = CountryLanguage.CountryCode;");
 	worldCandidateQueries23.add("select C.Name,CL.Language from Country C,CountryLanguage CL where C.Code=CL.CountryCode");
 	worldCandidateQueries23.add("select c.Name, cl.Language from Country c, CountryLanguage cl  where c.Code=cl.CountryCode");
@@ -114,7 +114,7 @@ public class CreateRankingHITs {
 	 */
 	ArrayList<String> worldCandidateQueries45 = new ArrayList<String>();
 	// without sample
-	worldCandidateQueries45.add("select Name from Country where code in(select CountryCode from CountryLanguage where Language like 'Swedish')"); // the query is syntactically right and can be rewritten, can we have its provenance? and do we need to rewrite the sub-SPJ?
+	//worldCandidateQueries45.add("select Name from Country where code in(select CountryCode from CountryLanguage where Language like 'Swedish')"); // the query is syntactically right and can be rewritten, can we have its provenance? and do we need to rewrite the sub-SPJ?
 	worldCandidateQueries45.add("select a.Name from Country a, CountryLanguage c where c.CountryCode = a.Code and c.Language = 'Swedish' and c.IsOfficial = 'T'");
 	worldCandidateQueries45.add("Select a.Name,b.IsOfficial from Country as a,CountryLanguage as b where a.Name='Swedish'");
 	worldCandidateQueries45.add("select Country.Name from Country,CountryLanguage  where (Country.Code = CountryLanguage.CountryCode) and (CountryLanguage.Language = 'Swedish') and (CountryLanguage.IsOfficial = 'T')"); // correct
@@ -125,7 +125,7 @@ public class CreateRankingHITs {
 	worldCandidateQueries45.add("Select Country.Name from Country, CountryLanguage where CountryLanguage.Language = 'Swedish' and Country.Code = CountryLanguage.CountryCode");
 	worldCandidateQueries45.add("SELECT Country.Name FROM Country, CountryLanguage WHERE CountryLanguage.CountryCode = Country.Code AND CountryLanguage.Language = 'Swedish' AND CountryLanguage.IsOfficial = 'T'");
 	// with sample
-	worldCandidateQueries45.add("select District from City where Population>3000000 and CountryCode in(select CountryCode from CountryLanguage where Language like 'Chinese')");
+	//worldCandidateQueries45.add("select District from City where Population>3000000 and CountryCode in(select CountryCode from CountryLanguage where Language like 'Chinese')");
 	worldCandidateQueries45.add("select b.Name from City b, CountryLanguage c where c.CountryCode = b.CountryCode and c.Language = 'Chinese' and b.Population > 3000000");
 	worldCandidateQueries45.add("select name from City where Population>=3000000;");
 	worldCandidateQueries45.add("select City.Name from City, CountryLanguage where (City.CountryCode = CountryLanguage.CountryCode) and (CountryLanguage.Language = 'Chinese') and (City.Population > 3000000)"); // the query is syntactically right and can be rewritten, can we have its provenance? and do we need to rewrite the sub-SPJ?
@@ -148,28 +148,25 @@ public class CreateRankingHITs {
 	 */
 	ArrayList<String> worldCandidateQueries67 = new ArrayList<String>();
 	// without sample
-	worldCandidateQueries67.add("select Name,Capital from Country where code in(select CountryCode from CountryLanguage where language like 'English' and Percentage<=50)"); // can rewrite it, and no syntax error... but the rewritten query is almost the same, because it has no foreign keys.
+	//worldCandidateQueries67.add("select Name,Capital from Country where code in(select CountryCode from CountryLanguage where language like 'English' and Percentage<=50)"); // can rewrite it, and no syntax error... but the rewritten query is almost the same, because it has no foreign keys.
 	worldCandidateQueries67.add("select a.Name,b.Name from Country a, City b, CountryLanguage c where a.Capital = b.ID and c.CountryCode = a.Code and c.Language = 'English' and c.Percentage >= 50");
 	worldCandidateQueries67.add("select Name, Capital from Country, CountryLanguage where (Country.Code = CountryLanguage.CountryCode) and (CountryLanguage.Language = 'English' and CountryLanguage.Percentage >= 50)");
 	worldCandidateQueries67.add("Select C.Name,C.Capital from Country C,CountryLanguage CL where CL.Language='English' and CL.Percentage>'50.0'");
 	worldCandidateQueries67.add("Select Country.Name, Country.Capital  From Country , CountryLanguage   Where CountryLanguage.Language = 'English' and CountryLanguage.Percentage >= 50  and CountryLanguage.CountryCode = Country.Code");
-	worldCandidateQueries67.add("select C.Name, Cy.Name from Country C, City Cy WHERE C.Capital = Cy.ID  AND EXISTS (SELECT * FROM CountryLanguage WHERE CountryCode = C.Code AND Language='English' AND Percentage>='50.0')"); // can rewrite it and no syntax error... don't need to rewrite the sub-SPJ because CountryLanguage has no foreign keys
+	//worldCandidateQueries67.add("select C.Name, Cy.Name from Country C, City Cy WHERE C.Capital = Cy.ID  AND EXISTS (SELECT * FROM CountryLanguage WHERE CountryCode = C.Code AND Language='English' AND Percentage>='50.0')"); // can rewrite it and no syntax error... don't need to rewrite the sub-SPJ because CountryLanguage has no foreign keys
 	worldCandidateQueries67.add("select c2.name Country, c1.name Capital from City c1, Country c2, CountryLanguage c3  where c1.id=c2.Capital and   c3.CountryCode= c2.Code and  c3.Language='English' and   c3.Percentage >=50");
-	worldCandidateQueries67.add("select Name, Capital from Country where Code in (select CountryCode from CountryLanguage where Language = 'English' and Percentage >= 50.0)");
+	//worldCandidateQueries67.add("select Name, Capital from Country where Code in (select CountryCode from CountryLanguage where Language = 'English' and Percentage >= 50.0)");
 	worldCandidateQueries67.add("SELECT C.Name,C.LocalName FROM City CC,Country C,CountryLanguage CCC WHERE CC.CountryCode=C.Code AND C.Code=CCC.CountryCode AND CCC.Language='English' AND C.Population > 50");
 	worldCandidateQueries67.add("SELECT Name ,Capital FROM Country , CountryLanguage where Language = 'English' and Percentage >= 50");
 	// with sample
-	worldCandidateQueries67.add("select Name,Population from City where Population<200000 and CountryCode in(select CountryCode from CountryLanguage where Language like 'Spanish')"); // can rewrite it, and no syntax error... but the rewritten query is almost the same, because it has no foreign keys.
+	//worldCandidateQueries67.add("select Name,Population from City where Population<200000 and CountryCode in(select CountryCode from CountryLanguage where Language like 'Spanish')"); // can rewrite it, and no syntax error... but the rewritten query is almost the same, because it has no foreign keys.
 	worldCandidateQueries67.add("select b.Name,b.Population from Country a ,City b, CountryLanguage c where a.Capital = b.ID and a.Continent = 'Europe' and a.Code = c.CountryCode and c.Language = 'Spanish' and c.IsOfficial = 'T'");
 	worldCandidateQueries67.add("select City.Name,City.Population from City, Country,CountryLanguage where (Country.Code = City.CountryCode) and (Country.Code = CountryLanguage.CountryCode) and  (CountryLanguage.Language = 'Spanish') and (Country.Continent = 'Europe') and (City.Population < 200000)");
 	worldCandidateQueries67.add("select C.Name,C.Population from City C,Country CU,CountryLanguage CL where CL.Language='Spanish' and CU.Region='Europe' and C.Population<200000");
 	worldCandidateQueries67.add("Select City.Name, City.Population  From City, Country, CountryLanguage   Where Country.Continent = 'Europe' and CountryLanguage.Language = 'Spanish' and Country.Population < 200000  and CountryLanguage.CountryCode = Country.Code and City.CountryCode = Country.Code");
-	/*
-	 * The sub-SPJ should be rewritten.
-	 */
-	worldCandidateQueries67.add("SELECT Cy.Name, Cy.Population FROM City Cy  WHERE Cy.CountryCode IN (Select C.Code FROM Country C, CountryLanguage CL WHERE C.Code = CL.CountryCode AND CL.IsOfficial='T' AND CL.Language='Spanish' AND C.Continent='Europe') AND Cy.Population<200000");
+	//worldCandidateQueries67.add("SELECT Cy.Name, Cy.Population FROM City Cy  WHERE Cy.CountryCode IN (Select C.Code FROM Country C, CountryLanguage CL WHERE C.Code = CL.CountryCode AND CL.IsOfficial='T' AND CL.Language='Spanish' AND C.Continent='Europe') AND Cy.Population<200000");
 	worldCandidateQueries67.add("select c1.name Name, c1.population Population  from City c1, Country c2, CountryLanguage c3  where c1.id=c2.Capital and  c3.CountryCode= c2.Code and  c2.Continent='Europe' and   c3.language='Spanish' and   c1.Population<200000");
-	worldCandidateQueries67.add("select Name, Population from City where CountryCode in (select CountryCode from CountryLanguage where Language = 'Spanish') and Population <= 200000");// can rewrite it and no syntax error... don't need to rewrite the sub-SPJ because CountryLanguage has no foreign keys
+	//worldCandidateQueries67.add("select Name, Population from City where CountryCode in (select CountryCode from CountryLanguage where Language = 'Spanish') and Population <= 200000");// can rewrite it and no syntax error... don't need to rewrite the sub-SPJ because CountryLanguage has no foreign keys
 	worldCandidateQueries67.add("SELECT C.Name,C.Population FROM City C,Country CC,CountryLanguage CL WHERE C.CountryCode=CC.Code AND CL.CountryCode=C.CountryCode AND CC.Continent='Europe' AND CL.Language='Spanish' AND CL.IsOfficial='T' AND  C.Population < 200000");
 	worldCandidateQueries67.add("Select Name , Population FROM Country , CountryLanguage where Continent = 'Europe' and Language = 'Spanish' and IsOfficial = 'T' and Population  < 200000");
 	String[] blockedRankerIdsWorld67 = {"A3988MX4PJCUJW", "A355UW5P1BF6M", 
@@ -256,11 +253,7 @@ public class CreateRankingHITs {
 	tpchCandidateQueries45.add("SELECT O_ORDERKEY, O_ORDERSTATUS FROM ORDERS, LINEITEM WHERE  L_ORDERKEY = O_ORDERKEY AND O_TOTALPRICE > 530000 AND L_DISCOUNT = 0");
 	tpchCandidateQueries45.add("select O_ORDERKEY,O_ORDERSTATUS FROM ORDERS O,LINEITEM L where (O_TOTALPRICE > 530000 AND L_DISCOUNT=0 AND O_ORDERKEY=L_ORDERKEY)");
 	tpchCandidateQueries45.add("select O.O_ORDERKEY,O.O_ORDERSTATUS from ORDERS O,LINEITEM LI where O_TOTALPRICE>530000 and LI.L_ORDERKEY=O.O_ORDERKEY and LI.L_DISCOUNT=0");
-	/*
-	 * In clause
-	 * cannot run?
-	 */
-	tpchCandidateQueries45.add("select O_CUSTKEY,O_ORDERSTATUS from ORDERS where O_TOTALPRICE<530000 in(select L_DISCOUNT from LINEITEM where L_DISCOUNT=0)");
+	//tpchCandidateQueries45.add("select O_CUSTKEY,O_ORDERSTATUS from ORDERS where O_TOTALPRICE<530000 in(select L_DISCOUNT from LINEITEM where L_DISCOUNT=0)");
 	tpchCandidateQueries45.add("SELECT O_ORDERKEY, O_ORDERSTATUS FROM ORDERS, LINEITEM WHERE O_TOTALPRICE>530000 AND L_DISCOUNT=0");
 	tpchCandidateQueries45.add("select L_ORDERKEY,O_ORDERSTATUS From ORDERS , LINEITEM  where L_ORDERKEY=O_ORDERKEY and  O_TOTALPRICE > 530000 and  L_DISCOUNT = 0");
 	tpchCandidateQueries45.add("SELECT ORDERS.O_ORDERKEY, ORDERS.O_ORDERSTATUS FROM ORDERS,LINEITEM WHERE ORDERS.O_TOTALPRICE>530000 AND LINEITEM.L_DISCOUNT=0 AND LINEITEM.L_ORDERKEY=ORDERS.O_ORDERKEY");
@@ -268,13 +261,10 @@ public class CreateRankingHITs {
 	tpchCandidateQueries45.add("select S_NAME , S_PHONE FROM SUPPLIER , NATION where N_NAME = 'CHINA' and S_ACCTBAL > 9000");
 	tpchCandidateQueries45.add("SELECT S_NAME, S_PHONE FROM SUPPLIER, NATION  WHERE S_ACCTBAL>9000  AND S_NATIONKEY=N_NATIONKEY  AND LOWER(N_NAME)='china'");
 	tpchCandidateQueries45.add("SELECT A.S_NAME,A.S_PHONE FROM SUPPLIER AS A,CUSTOMER AS B WHERE B.C_ADDRESS='CHINA' AND B.C_ACCTBAL>=9000");
-	/*
-	 * typo: but if we change s_PHONE to S_PHONE, it works!
-	 */
-	tpchCandidateQueries45.add("SELECT S_NAME, s_PHONE, S_ACCTBAL FROM SUPPLIER, NATION  WHERE N_NATIONKEY = s_NATIONKEY AND N_NATIONKEY = 'CHINA' AND s_ACCTBAL > 9000");
+	tpchCandidateQueries45.add("SELECT S_NAME, S_PHONE, S_ACCTBAL FROM SUPPLIER, NATION  WHERE N_NATIONKEY = S_NATIONKEY AND N_NATIONKEY = 'CHINA' AND S_ACCTBAL > 9000");
 	tpchCandidateQueries45.add("select S_NAME,S_PHONE from SUPPLIER S,NATION N where (S_ACCTBAL >= 9000 and N_NAME='CHINA' and S_NATIONKEY=N_NATIONKEY)");
-	tpchCandidateQueries45.add("select S.S_NAME,S.S_PHONE from SUPPLIER S  where S.S_ACCTBAL>9000 and S.S_NATIONKEY in (select N.N_NATIONKEY from NATION N,REGION R where R.R_NAME='CHINA' and N.N_REGIONKEY=R.R_REGIONKEY)"); // don't rewrite the sub-SPJ. However, the result (no matter rewritting it or not) is empty set
-	tpchCandidateQueries45.add("select S_NAME,S_PHONE from SUPPLIER where S_ACCTBAL>9000 in(select N_NATIONKEY from NATION where N_NAME like 'CHINA')"); // don't rewrite sub-SPJ, can rewrite and run
+	//tpchCandidateQueries45.add("select S.S_NAME,S.S_PHONE from SUPPLIER S  where S.S_ACCTBAL>9000 and S.S_NATIONKEY in (select N.N_NATIONKEY from NATION N,REGION R where R.R_NAME='CHINA' and N.N_REGIONKEY=R.R_REGIONKEY)"); // don't rewrite the sub-SPJ. However, the result (no matter rewritting it or not) is empty set
+	//tpchCandidateQueries45.add("select S_NAME,S_PHONE from SUPPLIER where S_ACCTBAL>9000 in(select N_NATIONKEY from NATION where N_NAME like 'CHINA')"); // don't rewrite sub-SPJ, can rewrite and run
 	tpchCandidateQueries45.add("SELECT S_NAME, S_PHONE FROM SUPPLIER, NATION WHERE N_NAME='CHINA' AND S_ACCTBAL>9000");
 	tpchCandidateQueries45.add("select S_NAME,S_PHONE from SUPPLIER ,NATION   where N_NATIONKEY=S_NATIONKEY and  N_NAME = 'CHINA' and  S_ACCTBAL > 9000");
 	tpchCandidateQueries45.add("SELECT SUPPLIER.S_NAME, SUPPLIER.S_PHONE FROM SUPPLIER, NATION  WHERE NATION.N_NATIONKEY=SUPPLIER.S_NATIONKEY AND NATION.N_NAME='CHINA' AND SUPPLIER.S_ACCTBAL>9000");
@@ -293,8 +283,8 @@ public class CreateRankingHITs {
 	tpchCandidateQueries67.add("SELECT C_NAME , C_ACCTBAL FROM CUSTOMER , NATION  WHERE N_NAME ='EUROPE' AND C_ACCTBAL > 9990");
 	tpchCandidateQueries67.add("SELECT C_NAME, C_ACCTBAL FROM CUSTOMER C, NATION N, REGION R  WHERE C.C_ACCTBAL>9990  AND C.c_NATIONKEY=N.N_NATIONKEY  AND N.N_REGIONKEY=R.R_REGIONKEY  AND lower(R_NAME)='europe'"); // good query
 	tpchCandidateQueries67.add("SELECT C_NAME,C_ACCTBAL FROM CUSTOMER WHERE C_ADDRESS='EUROPE'  AND C_ACCTBAL>=9990;");
-	tpchCandidateQueries67.add("select C_NAME, C_ACCTBAL   from CUSTOMER  where C_NATIONKEY in  (select N_NATIONKEY from NATION where N_NAME = 'EUROPE')"); // don't rewrite sub-SPJ, but empty set no matter rewritting it or not...
-	tpchCandidateQueries67.add("select C.C_NAME,C.C_ACCTBAL from CUSTOMER C where C_NATIONKEY in (select N.N_NATIONKEY from NATION N,REGION R where R.R_NAME='EUROPE') and C.C_ACCTBAL>9990");
+	//tpchCandidateQueries67.add("select C_NAME, C_ACCTBAL   from CUSTOMER  where C_NATIONKEY in  (select N_NATIONKEY from NATION where N_NAME = 'EUROPE')"); // don't rewrite sub-SPJ, but empty set no matter rewritting it or not...
+	//tpchCandidateQueries67.add("select C.C_NAME,C.C_ACCTBAL from CUSTOMER C where C_NATIONKEY in (select N.N_NATIONKEY from NATION N,REGION R where R.R_NAME='EUROPE') and C.C_ACCTBAL>9990");
 	tpchCandidateQueries67.add("select C_NAME,C_ACCTBAL from CUSTOMER where C_NATIONKEY like 'EUROPE' and C_ACCTBAL>9990");
 	tpchCandidateQueries67.add("SELECT CUSTOMER.C_NAME,NATION.N_NAME FROM CUSTOMER,NATION WHERE NATION.N_NAME='EUROPE' AND CUSTOMER.C_ACCTBAL>9990");
 	tpchCandidateQueries67.add("SELECT C_NAME,C_ACCTBAL FROM CUSTOMER WHERE C_ADDRESS = 'EUROPE' AND C_ACCTBAL > 9990");
@@ -304,9 +294,9 @@ public class CreateRankingHITs {
 	tpchCandidateQueries67.add("SELECT S_NAME , S_PHONE FROM SUPPLIER , PART WHERE S_ACCTBAL > 1000 AND 	P_RETAILPRICE > 2096");
 	tpchCandidateQueries67.add("SELECT S_NAME, S_PHONE FROM SUPPLIER, PARTSUPP WHERE PS_SUPPKEY=S_SUPPKEY  AND S_ACCTBAL>1000  AND PS_SUPPLYCOST>2096");
 	tpchCandidateQueries67.add("SELECT A.S_NAME,A.S_PHONE FROM SUPPLIER AS A,PART AS B WHERE A.S_ACCTBAL>1000 AND B.P_RETAILPRICE>2096;");
-	tpchCandidateQueries67.add("select S_NAME, S_PHONE from SUPPLIER  where (S_ACCTBAL > '1000' and   S_SUPPKEY in   (select PS_SUPPKEY from PARTSUPP where PS_PARTKEY in (  select P_PARTKEY from PART where P_RETAILPRICE > '2096')))"); // nested too much...
-	tpchCandidateQueries67.add("select S.S_NAME,S.S_PHONE from SUPPLIER S where S.S_ACCTBAL>1000.0 and S.S_SUPPKEY in (select PS.PS_SUPPKEY from PARTSUPP PS,PART P where P.P_PARTKEY=PS.PS_PARTKEY and P.P_RETAILPRICE>2096.0)"); // nested
-	tpchCandidateQueries67.add("select S_NAME,S_PHONE from SUPPLIER WHERE S_ACCTBAL>1000 in (select PS_SUPPKEY from PARTSUPP where PS_SUPPLYCOST>2096)"); // nested
+	//tpchCandidateQueries67.add("select S_NAME, S_PHONE from SUPPLIER  where (S_ACCTBAL > '1000' and   S_SUPPKEY in   (select PS_SUPPKEY from PARTSUPP where PS_PARTKEY in (  select P_PARTKEY from PART where P_RETAILPRICE > '2096')))"); // nested too much...
+	//tpchCandidateQueries67.add("select S.S_NAME,S.S_PHONE from SUPPLIER S where S.S_ACCTBAL>1000.0 and S.S_SUPPKEY in (select PS.PS_SUPPKEY from PARTSUPP PS,PART P where P.P_PARTKEY=PS.PS_PARTKEY and P.P_RETAILPRICE>2096.0)"); // nested
+	//tpchCandidateQueries67.add("select S_NAME,S_PHONE from SUPPLIER WHERE S_ACCTBAL>1000 in (select PS_SUPPKEY from PARTSUPP where PS_SUPPLYCOST>2096)"); // nested
 	tpchCandidateQueries67.add("SELECT S_NAME,S_PHONE FROM SUPPLIER WHERE S_ACCTBAL >1000");
 	tpchCandidateQueries67.add("SELECT S_NAME,S_PHONE FROM SUPPLIER, PARTSUPP WHERE SUPPLIER.S_ACCTBAL > 1000 AND PARTSUPP.PS_SUPPLYCOST > 2096");
 	tpchCandidateQueries67.add("SELECT S.S_NAME, S.S_PHONE  FROM SUPPLIER  S, PART P, PARTSUPP PS  WHERE S_ACCTBAL > 1000   AND P.P_PARTKEY = PS.PS_PARTKEY  AND S.S_SUPPKEY = PS.PS_SUPPKEY  AND P_RETAILPRICE > 2096");
