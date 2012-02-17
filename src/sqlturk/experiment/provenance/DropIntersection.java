2ed34bee21cf416db77ffd3c5de474d2831b41b5
khuang1024 <khuang1024@gmail.com>
2012-02-17 15:17:32 -0800

    Added classes for dropping tables.

---
+++ src/sqlturk/experiment/provenance/DropIntersection.java
@@ -0,0 +1,58 @@
+package sqlturk.experiment.provenance;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import sqlturk.configuration.Parameters;
+
+public class DropIntersection {
+
+    /**
+     * @param args
+     * @throws SQLException 
+     */
+    public static void main(String[] args) throws SQLException {
+	try {
+	    Class.forName("com.mysql.jdbc.Driver").newInstance();
+	    Connection dbConn;
+
+	    // use soe server or test locally
+	    if (Parameters.USE_SERVER) {
+		dbConn = DriverManager
+			.getConnection(Parameters.MYSQL_CONNECTION_STRING);
+		System.out.println("Using server.");
+	    } else {
+		dbConn = DriverManager.getConnection(Parameters.LOCAL_DB_URL,
+			Parameters.LOCAL_USER, Parameters.LOCAL_PASSWORD);
+		System.out.println("Using local.");
+	    }
+	    
+	    Statement stmt = dbConn.createStatement();
+	    ResultSet rs = stmt.executeQuery("SHOW TABLES");
+	    ArrayList<String> intersections = new ArrayList<String>();
+	    while (rs.next()) {
+		String tableName = rs.getString(1);
+		if (tableName.endsWith(Parameters.INTERSECTION_REL_NAME)) {
+		    intersections.add(tableName);
+		}
+	    }
+	    for (String intersection : intersections) {
+		stmt.executeUpdate("DROP TABLE " + intersection);
+	    }
+	    
+	    
+	} catch (InstantiationException e) {
+	    e.printStackTrace();
+	} catch (IllegalAccessException e) {
+	    e.printStackTrace();
+	} catch (ClassNotFoundException e) {
+	    e.printStackTrace();
+	}
+
+    }
+
+}
---
+++ src/sqlturk/experiment/provenance/DropUnion.java
@@ -0,0 +1,58 @@
+package sqlturk.experiment.provenance;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import sqlturk.configuration.Parameters;
+
+public class DropUnion {
+
+    /**
+     * @param args
+     * @throws SQLException 
+     */
+    public static void main(String[] args) throws SQLException {
+	try {
+	    Class.forName("com.mysql.jdbc.Driver").newInstance();
+	    Connection dbConn;
+
+	    // use soe server or test locally
+	    if (Parameters.USE_SERVER) {
+		dbConn = DriverManager
+			.getConnection(Parameters.MYSQL_CONNECTION_STRING);
+		System.out.println("Using server.");
+	    } else {
+		dbConn = DriverManager.getConnection(Parameters.LOCAL_DB_URL,
+			Parameters.LOCAL_USER, Parameters.LOCAL_PASSWORD);
+		System.out.println("Using local.");
+	    }
+	    
+	    Statement stmt = dbConn.createStatement();
+	    ResultSet rs = stmt.executeQuery("SHOW TABLES");
+	    ArrayList<String> unions = new ArrayList<String>();
+	    while (rs.next()) {
+		String tableName = rs.getString(1);
+		if (tableName.endsWith(Parameters.UNION_REL_NAME)) {
+		    unions.add(tableName);
+		}
+	    }
+	    for (String union : unions) {
+		stmt.executeUpdate("DROP TABLE " + union);
+	    }
+	    
+	    
+	} catch (InstantiationException e) {
+	    e.printStackTrace();
+	} catch (IllegalAccessException e) {
+	    e.printStackTrace();
+	} catch (ClassNotFoundException e) {
+	    e.printStackTrace();
+	}
+
+    }
+
+}
