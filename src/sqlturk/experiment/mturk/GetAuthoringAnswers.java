85d819d7143c803b096757548481796d154923a8
khuang1024 <khuang1024@gmail.com>
2012-02-20 19:11:09 -0800

    Retrieved new answers.

--- src/sqlturk/experiment/mturk/GetAuthoringAnswers.java
+++ src/sqlturk/experiment/mturk/GetAuthoringAnswers.java
@@ -42,9 +42,7 @@ public class GetAuthoringAnswers {
 	    System.out.println("Using local.");
 	}
 	
-	AuthoringManager.downloadHitAnswers(service, "2TYW40SPW1INGZ2ODLA3RAB7GVI1E7", dbConn);
-//	AuthoringManager.forceDownloadHitAnswers(service, "2ZFKO2L92HECQSSNQFFCGF0DS58GFY", dbConn);
-//	AuthoringManager.forceDownloadHitAnswers(service, "20R2NJKM05BMDBG7LDTGXP9SD7GBGB", dbConn);
+	AuthoringManager.downloadHitAnswers(service, "2M998D8J3VE7QAEJAF3G7MCLDQPHXV", dbConn);
     }
     
 
