76e7cc3a1381b0c395d6609445d3136b419e967d
khuang1024 <khuang1024@gmail.com>
2012-02-26 20:16:06 -0800

    Fixed #12 -- Fixed "column match" problem for union
    Fixed the "column match" problem for union operation. If the
    ORIGINAL columns of each resulting table do not match, then the
    union table is null.

--- src/sqlturk/util/union/Union.java
+++ src/sqlturk/util/union/Union.java
@@ -29,8 +29,12 @@ public class Union {
 	ArrayList<String> allProjectedRewriteResultTables = Projection
 		.getAllProjectedRewriteResultTables(dbConn);
 
-	// when there is no common attribute for resulting tuples
-	if (allProjectedRewriteResultTables.isEmpty()) {
+ 	if (allProjectedRewriteResultTables == null) {
+	    stmt.executeUpdate("CREATE TABLE " + Parameters.UNION_REL_NAME
+		    + " (INFO VARCHAR (30)) ");
+	    stmt.executeUpdate("INSERT INTO " + Parameters.UNION_REL_NAME
+		    + " VALUES ('Columns dismatch')");
+	} else 	if (allProjectedRewriteResultTables.isEmpty()) {// when there is no common attribute for resulting tuples
 	    stmt.executeUpdate("CREATE TABLE " + Parameters.UNION_REL_NAME
 		    + " (INFO VARCHAR (30)) ");
 	    stmt.executeUpdate("INSERT INTO " + Parameters.UNION_REL_NAME
