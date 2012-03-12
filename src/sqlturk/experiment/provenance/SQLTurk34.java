e8f0bb90887f75973526d77be0f5bb1738f12d66
khuang1024 <khuang1024@gmail.com>
2012-03-12 10:41:16 -0700

    Added another legal value 'all' for argument type.

--- src/sqlturk/experiment/provenance/SQLTurk34.java
+++ src/sqlturk/experiment/provenance/SQLTurk34.java
@@ -64,22 +64,9 @@ public class SQLTurk34 {
     }
     
     private static void compute(String type, String datasetName, int queryIndex, int topN, 
-	    int[] candidates, int limit) throws SQLException {
+	    int[] candidates, int limit, Connection dbConn) throws SQLException {
 	Parameters.FD_REL_NAME = getFDName(datasetName, queryIndex, topN, candidates);
 	Parameters.FD_PLUS_REL_NAME = getFDPlusName(datasetName, queryIndex, topN, candidates);
-	
-	try {
-	    Class.forName("com.mysql.jdbc.Driver").newInstance();
-	    Connection dbConn;
-
-	    // use soe server or test locally
-	    if (Parameters.USE_SERVER) {
-		dbConn = DriverManager.getConnection(Parameters.MYSQL_CONNECTION_STRING);
-		System.out.println("Using server.");
-	    } else {
-		dbConn = DriverManager.getConnection(Parameters.LOCAL_DB_URL, Parameters.LOCAL_USER, Parameters.LOCAL_PASSWORD);
-		System.out.println("Using local.");
-	    }
 
 	    // create the standard answer result table
 	    String standardAnswer = QueryManager.getStandardAnswer(datasetName, queryIndex);
@@ -101,9 +88,9 @@ public class SQLTurk34 {
 	    }
 
 	    // clean old tables
-	    System.out.println("Start clearing old tables ......");
+	    System.out.println("Start clearing old intermediate tables ......");
 	    Cleaner.dropAll(dbConn);
-	    System.out.println("Finish clearing old tables.\n");
+	    System.out.println("Finish clearing old intermediate tables.\n");
 
 
 	    // rewrite queries
@@ -112,7 +99,8 @@ public class SQLTurk34 {
 	    System.out.println("Start rewriting queries ...............");
 	    tempQueries = TableauxRewriter.getRewriteQueries(originalQueries, dbConn);
 	    for (int i = 0; i < tempQueries.size(); i++) {
-		rewriteQueries.add(tempQueries.get(i) + " ORDER BY RAND() LIMIT " + limit);
+		rewriteQueries.add(tempQueries.get(i) + " LIMIT " + limit);
+//		rewriteQueries.add(tempQueries.get(i) + " ORDER BY RAND() LIMIT " + limit);
 	    }
 	    System.out.println("Finish rewriting queries.\n");
 
@@ -169,24 +157,58 @@ public class SQLTurk34 {
 		String result = header + ", FDP_Plus, Precision=" + fdplusStd + ", Recall=" + stdFDPlus;
 		System.out.println(result);
 		appendToLog(result, Parameters.PERFORMANCE_LOG_NAME);
+	    } else if (type.equals("all")) {
+		System.out.println("Start creating " + Parameters.INTERSECTION_REL_NAME + " .............");
+		Intersection.createIntersectionRelation(dbConn);
+		System.out.println("Finish creating " + Parameters.INTERSECTION_REL_NAME + ".\n");
+		double stdIntersec = Metric.sim(Parameters.STANDARD_ANSWER_REL_NAME, Parameters.INTERSECTION_REL_NAME, dbConn);
+		double intersecStd = Metric.sim(Parameters.INTERSECTION_REL_NAME, Parameters.STANDARD_ANSWER_REL_NAME, dbConn);
+		String result = header + ", Intersection, Precision=" + intersecStd + ", Recall=" + stdIntersec;
+		System.out.println(result);
+		appendToLog(result, Parameters.PERFORMANCE_LOG_NAME);
+		
+		System.out.println("Start creating " + Parameters.UNION_REL_NAME + " ............");
+		Union.createUionRelation(dbConn);
+		System.out.println("Finish creating " + Parameters.UNION_REL_NAME + ".\n");
+		double stdUnion = Metric.sim(Parameters.STANDARD_ANSWER_REL_NAME, Parameters.UNION_REL_NAME, dbConn);
+		double unionStd = Metric.sim(Parameters.UNION_REL_NAME, Parameters.STANDARD_ANSWER_REL_NAME, dbConn);
+		result = header + ", Union, Precision=" + unionStd + ", Recall=" + stdUnion;
+		System.out.println(result);
+		appendToLog(result, Parameters.PERFORMANCE_LOG_NAME);
+		
+		System.out.println("Start creating " + Parameters.FD_REL_NAME);
+		FD.createFDRelationNormally(dbConn);
+//		FD.createFDRelationOptimally(datasetName, queryIndex, topN, candidates, dbConn);
+		System.out.println("Finish creating " + Parameters.FD_REL_NAME + ".\n");
+		double stdFD = Metric.sim(Parameters.STANDARD_ANSWER_REL_NAME, Parameters.FD_REL_NAME, dbConn);
+		double fdStd = Metric.sim(Parameters.FD_REL_NAME, Parameters.STANDARD_ANSWER_REL_NAME, dbConn);
+		result = header + ", FD, Precision=" + fdStd + ", Recall=" + stdFD;
+		System.out.println(result);
+		appendToLog(result, Parameters.PERFORMANCE_LOG_NAME);
+		
+		System.out.println("Start creating " + Parameters.PROV_REL_NAME);
+		Provenance.createWhyProvenanceRelation(rewriteQueries, dbConn); // create PROV table
+		System.out.println("Finish creating " + Parameters.PROV_REL_NAME + ".\n");
+		System.out.println("Start creating " + Parameters.CONN_REL_NAME);
+		Connected.createWhyConnectedRelation(dbConn); // create CONN table
+		System.out.println("Finish creating " + Parameters.CONN_REL_NAME + ".\n");
+		System.out.println("Start creating " + Parameters.FD_PLUS_REL_NAME);
+		FDPlus.createFDPlusRelation(dbConn);
+		System.out.println("Finish creating " + Parameters.FD_PLUS_REL_NAME + ".\n");
+		double stdFDPlus = Metric.sim(Parameters.STANDARD_ANSWER_REL_NAME, Parameters.FD_PLUS_REL_NAME, dbConn);
+		double fdplusStd = Metric.sim(Parameters.FD_PLUS_REL_NAME, Parameters.STANDARD_ANSWER_REL_NAME, dbConn);
+		result = header + ", FDP_Plus, Precision=" + fdplusStd + ", Recall=" + stdFDPlus;
+		System.out.println(result);
+		appendToLog(result, Parameters.PERFORMANCE_LOG_NAME);
 	    } else {
 		throw new RuntimeException("Wrong type.");
 	    }
 	    
-	    System.out.println("Start clearing tables ............");
+	    System.out.println("Start clearing intermediate tables ............");
 	    Cleaner.dropAll(dbConn);
-	    System.out.println("Finish clearing tables.\n");
+	    System.out.println("Finish clearing intermediate tables.\n");
 
-	    dbConn.close();
 	    System.out.println("Done.");
-	    
-	} catch (InstantiationException e) {
-	    e.printStackTrace();
-	} catch (IllegalAccessException e) {
-	    e.printStackTrace();
-	} catch (ClassNotFoundException e) {
-	    e.printStackTrace();
-	}
     }
 
 
@@ -207,11 +229,33 @@ public class SQLTurk34 {
 	    }
 	}
 	
-	compute(type, datasetName, queryIndex, topN, candidates, limit);
-	
-//	for (int topN = 2; topN <= candidates.length; topN++) {
-//	    compute(datasetName, queryIndex, topN, candidates, limit);
-//	}
+	Connection dbConn = null;
+	try {
+	    Class.forName("com.mysql.jdbc.Driver").newInstance();
+	    
+	    // use soe server or test locally
+	    if (Parameters.USE_SERVER) {
+		dbConn = DriverManager.getConnection(Parameters.MYSQL_CONNECTION_STRING);
+		System.out.println("Using server.");
+	    } else {
+		dbConn = DriverManager.getConnection(Parameters.LOCAL_DB_URL, Parameters.LOCAL_USER, Parameters.LOCAL_PASSWORD);
+		System.out.println("Using local.");
+	    }
+	    
+	    compute(type, datasetName, queryIndex, topN, candidates, limit, dbConn);
+	    
+	    
+	} catch (InstantiationException e) {
+	    e.printStackTrace();
+	} catch (IllegalAccessException e) {
+	    e.printStackTrace();
+	} catch (ClassNotFoundException e) {
+	    e.printStackTrace();
+	} finally {
+	    if (dbConn != null) {
+		dbConn.close();
+	    }
+	}
     }
-
+    
 }
